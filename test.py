4e2d8c2714ecb0c462886301529baaa4f0ef68b4
coltonlloyd <cjlloyd@eng.ucsd.edu>
2017-04-17 16:49:05 -0700

    First steps to soplex 3 / python 3 compatibility

--- test.py
+++ test.py
@@ -3,11 +3,11 @@ import cobra
 cobra.solvers.solver_dict = {"soplex": soplex}
 
 import cobra.test
-cobra.test.solvers.test_all()
+#cobra.test.solvers.test_all()
 
 m = cobra.test.create_test_model()
 
 a = soplex.Soplex(m)
 s = soplex.solve(m)
-print repr(s)
+print(repr(s))
 #from IPython import embed; embed()
